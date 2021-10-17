160f558df7cffaf2ea9173b67a447214e40336ef
jalil <jalil@1001.digital>
2021-10-17 15:26:10 +0200

    Before timestamp

--- watchSales.ts
+++ watchSales.ts
@@ -57,7 +57,7 @@ async function main() {
   const channel = await discordSetup();
   let lastSale = (await fetchLastSales({
     limit: '1',
-    // occurred_before: '1634475000'
+    occurred_before: '1634475000'
   }))[0]
   let afterLastSale = Date.parse(`${lastSale?.transaction.timestamp}Z`) / 1000 + 1 // +1 second
 
