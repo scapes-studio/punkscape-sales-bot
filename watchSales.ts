937174339e809a4eb15666aa58b37c9d11f23524
jalil <jalil@1001.digital>
2021-10-17 15:46:27 +0200

    Add msgs

--- watchSales.ts
+++ watchSales.ts
@@ -57,7 +57,7 @@ async function main() {
   const channel = await discordSetup();
   let lastSale = (await fetchLastSales({
     limit: '1',
-    // occurred_before: '1634475000'
+    occurred_before: '1634475000'
   }))[0]
   let afterLastSale = Date.parse(`${lastSale?.transaction.timestamp}Z`) / 1000 + 1 // +1 second
 
@@ -77,6 +77,7 @@ async function main() {
     await Promise.all([
       ...salesSince?.reverse().map(async (sale: any) => {
         const message = buildMessage(sale);
+        if (! message) return
         return channel.send(message)
       }),
       // Wait until the next iteration
