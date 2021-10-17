497a28be4c13023d9c48b1d0ad23ef15532bcbe6
jalil <jalil@1001.digital>
2021-10-17 14:07:07 +0200

    Add sale watcher

---
+++ watchSales.ts
@@ -0,0 +1,87 @@
+import 'dotenv/config';
+import Discord, { TextChannel } from 'discord.js';
+import fetch from 'node-fetch';
+import { ethers } from 'ethers';
+import shortAddress from './helpers/short-address';
+import { delay } from './helpers/time';
+
+const discordBot = new Discord.Client();
+const discordSetup = async (): Promise<TextChannel> => {
+  return new Promise<TextChannel>((resolve, reject) => {
+    ['DISCORD_BOT_TOKEN', 'DISCORD_CHANNEL_ID'].forEach((envVar) => {
+      if (!process.env[envVar]) reject(`${envVar} not set`)
+    })
+
+    discordBot.login(process.env.DISCORD_BOT_TOKEN);
+    discordBot.on('ready', async () => {
+      const channel = await discordBot.channels.fetch(process.env.DISCORD_CHANNEL_ID!);
+      resolve(channel as TextChannel);
+    });
+  })
+}
+
+const buildMessage = (sale: any) => {
+  const buyer = sale?.winner_account?.user?.username || shortAddress(sale?.winner_account?.address)
+  const price = ethers.utils.formatEther(sale.total_price || '0')
+  const usdPrice = (parseFloat(price) * parseFloat(sale?.payment_token?.usd_price || 3200))
+    .toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})
+
+  return new Discord.MessageEmbed()
+      .setColor('#eeeeee')
+      .setTitle(sale.asset.name + ' sold')
+      .setURL(sale.asset.permalink)
+      .addFields(
+        { name: 'Buyer', value: `[${buyer}](https://opensea.io/${sale?.winner_account?.address})`, inline: true },
+        { name: 'Amount', value: `${price} ${ethers.constants.EtherSymbol} ($${usdPrice} USD)`, inline: true },
+      )
+      .setImage(sale.asset.image_url)
+}
+
+async function fetchLastSales(queryParams) {
+  const params = new URLSearchParams({
+    offset: '0',
+    event_type: 'successful',
+    only_opensea: 'false',
+    limit: '30',
+    collection_slug: process.env.COLLECTION_SLUG!,
+    asset_contract_address: process.env.CONTRACT_ADDRESS!,
+    ...queryParams,
+  })
+
+  const openSeaResponse = await (await fetch(`https://api.opensea.io/api/v1/events?${params}`)).json()
+
+  return openSeaResponse?.asset_events
+}
+
+async function main() {
+  const channel = await discordSetup();
+  let lastSale = (await fetchLastSales({ limit: '1' }))[0]
+  let afterLastSale = Date.parse(`${lastSale?.transaction.timestamp}Z`) / 1000 + 1 // +1 second
+
+  while (true) {
+    await delay(parseInt(process.env.SECONDS! || '60') * 1000)
+    let salesSince = await fetchLastSales({ occurred_after: afterLastSale.toString() })
+
+    if (!salesSince.length) {
+      console.info(`No last sales since ${afterLastSale}`)
+      continue
+    }
+
+    lastSale = salesSince[0]
+    afterLastSale = Date.parse(`${lastSale?.transaction.timestamp}Z`) / 1000 + 1
+    console.info(`New last sale: #${lastSale.asset.token_id} - ${salesSince.length} fetched in total`)
+
+    await Promise.all(
+      salesSince?.reverse().map(async (sale: any) => {
+        const message = buildMessage(sale);
+        return channel.send(message)
+      })
+    );
+  }
+}
+
+main()
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
