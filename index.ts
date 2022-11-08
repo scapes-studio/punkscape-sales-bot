5469ba5edd8479b26ee2a019ff1bcc73d3b57e37
jalil <jalil@1001.digital>
2022-11-08 11:00:42 +0100

    Add ODP and G27 bots

--- index.ts
+++ index.ts
@@ -1,14 +1,67 @@
 import 'dotenv/config'
 import watchSales from './tasks/watch-sales'
 import { discordSetup } from './helpers/discord'
+import SCAPE_DATA from './provenance-metadata.json'
+import { delay } from './helpers/time'
+import fetch from 'node-fetch'
 
 const init = async () => {
   await discordSetup()
 
   watchSales({
-    contract: process.env.PUNKSCAPES_CONTRACT_ADDRESS,
+    contract: [ process.env.PUNKSCAPES_CONTRACT_ADDRESS ],
     name: 'Scape',
-    from: process.env.START,
+    from: process.env.START_SCAPES,
+    discord: {
+      fields: config => [
+        { name: 'Scapoor', value: `[${config.buyer}](https://punkscape.xyz/accounts/${config.sale.to})`, inline: true },
+        { name: 'Price', value: config.priceString, inline: true },
+        { name: 'Gallery 27 Date', value: `[${SCAPE_DATA[config.ID].date}](https://punkscape.xyz/gallery27/punkscape/${config.ID})`, inline: true },
+      ],
+      image: async config => `https://ipfs.punkscape.xyz/ipfs/QmaADSyXfiNhTqLKtRYxvGB4qJ7ZAR375Yh48SiCmcVYYE/${config.ID}/image.png`,
+    },
+    url: config => `https://punkscape.xyz/scapes/${config.ID}`,
+    tweet: config =>
+      `Scape #${config.ID} was just bought by ${config.buyer} for ${config.priceString}. \n\nIts Gallery 27 date is ${SCAPE_DATA[config.ID].date}\n\nhttps://punkscape.xyz/scapes/${config.ID}`,
+  })
+
+  await delay(10 * 1000) // wait 10 seconds
+  watchSales({
+    contract: [ process.env.ONEDAYPUNKS_CONTRACT_ADDRESS ],
+    name: 'One Day Punk',
+    from: process.env.START_ODP,
+    discord: {
+      title: config => `Welcome One Day Punk #${config.ID}`,
+      thumbnail: config => `https://api.punkscape.xyz/onedaypunks/${config.ID}/profile.png`,
+      color: '#ffffff',
+      fields: config => [
+        { name: 'Punk', value: `[${config.buyer}](https://punkscape.xyz/accounts/${config.sale.to})`, inline: true },
+        { name: 'Price', value: config.priceString, inline: true },
+      ],
+    },
+    url: config => `https://punkscape.xyz/onedaypunks/${config.ID}`,
+  })
+
+  await delay(10 * 1000) // wait 10 seconds
+  watchSales({
+    contract: [ process.env.TWENTYSEVENYEAR_SCAPES_CONTRACT_ADDRESS ],
+    name: 'Twenty Seven Year Scape',
+    from: process.env.START_27YSCAPES,
+    discord: {
+      fields: config => [
+        { name: 'Scapoor', value: `[${config.buyer}](https://punkscape.xyz/accounts/${config.sale.to})`, inline: true },
+        { name: 'Price', value: config.priceString, inline: true },
+      ],
+      image: async config => {
+        const response = await fetch(`https://api.punkscape.xyz/gallery27/scapes/${config.ID}`)
+        const scape = await response.json()
+
+        return scape.imageRequest?.image?.image_url
+      },
+    },
+    url: config => `https://punkscape.xyz/gallery27/${config.ID}`,
+    tweet: config =>
+      `Twenty Seven Year Scape #${config.ID} was bought by ${config.buyer} for ${config.priceString}. \n\nhttps://punkscape.xyz/gallery27/${config.ID}`,
   })
 }
 
--- tasks/watch-sales.ts
+++ tasks/watch-sales.ts
@@ -1,17 +1,32 @@
-import { MessageEmbed, TextChannel } from 'discord.js'
+import { ColorResolvable, EmbedFieldData, MessageEmbed, TextChannel } from 'discord.js'
 import sdk from './../helpers/api'
 import { paths } from '@reservoir0x/reservoir-kit-client'
 import shortAddress from '../helpers/short-address'
-import SCAPE_DATA from './../provenance-metadata.json'
 import { CHANNELS } from '../helpers/discord'
 import { sendTweet } from '../helpers/twitter'
 import { delay, now } from '../helpers/time'
 
 type Sale = paths['/sales/v4']['get']['responses']['200']['schema']['sales'][0]
+type CustomSale = {
+  ID: string,
+  priceString: string;
+  buyer: string;
+  sale: Sale;
+}
 type SaleWatcherDefinition = {
-  contract: string;
+  contract: string[];
   name: string;
   from?: string;
+  tweet?: (config: CustomSale) => string;
+  discord?: {
+    color?: ColorResolvable;
+    title?: (config: CustomSale) => string;
+    fields?: (config: CustomSale) => EmbedFieldData[];
+    image?: (config: CustomSale) => Promise<string>;
+    thumbnail?: (config: CustomSale) => string;
+  }
+  url?: (config: CustomSale) => string;
+  imageURL?: (config: CustomSale) => Promise<string>;
 }
 
 const watchSales = async (config: SaleWatcherDefinition) => {
@@ -22,7 +37,7 @@ const watchSales = async (config: SaleWatcherDefinition) => {
       const sales = await fetchSales(config.contract, since, (currentTime - 1).toString())
       console.log(`Found ${sales.length} new sales events since ${since}`)
       for (const sale of sales) {
-        await reportSale(sale)
+        await reportSale(sale, config)
       }
       since = currentTime.toString()
       await delay(60 * 1000) // Wait for one minute
@@ -32,10 +47,10 @@ const watchSales = async (config: SaleWatcherDefinition) => {
   }
 }
 
-const fetchSales = async (contract: string, from: string, to: string) => {
+const fetchSales = async (contract: string[], from: string, to: string) => {
   try {
     const { data } = await sdk.getSalesV4({
-      contract: contract,
+      contract,
       startTimestamp: from,
       endTimestamp: to,
       limit: '1000',
@@ -48,29 +63,34 @@ const fetchSales = async (contract: string, from: string, to: string) => {
   }
 }
 
-export const reportSale = async (sale: Sale) => {
+export const reportSale = async (sale: Sale, config: SaleWatcherDefinition) => {
   const buyer = shortAddress(sale.to)
   const price = sale.price.amount.native
   const usdPrice = sale.price.amount.usd?.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })
   const priceString = `${price} ${sale.price.currency.name} ($${usdPrice} USD)`
 
   const ID = sale.token.tokenId
-  const date = SCAPE_DATA[ID].date
+  const saleData: CustomSale = { ID, buyer, priceString, sale }
 
   // Tweet
-  sendTweet(`Scape #${sale.token.tokenId} was just bought by ${buyer} for ${priceString}. \n\nIts Gallery 27 date is ${date}\n\nhttps://punkscape.xyz/scapes/${ID}`)
+  if (config.tweet) {
+    sendTweet(config.tweet(saleData))
+  }
 
   // DiscordMessage
+  const title = typeof config.discord?.title === 'function'
+    ? config.discord?.title(saleData)
+    : `${config.name} #${sale.token.tokenId} has a new owner`
+  const color = config.discord?.color || '#eeeeee'
+
   const discordMessage = new MessageEmbed()
-      .setColor('#eeeeee')
-      .setTitle(`Scape #${sale.token.tokenId} has a new owner`)
-      .setURL(`https://punkscape.xyz/scapes/${ID}`)
-      .addFields(
-        { name: 'Scapoor', value: `[${buyer}](https://punkscape.xyz/accounts/${sale.to})`, inline: true },
-        { name: 'Price', value: priceString, inline: true },
-        { name: 'Gallery 27 Date', value: `[${date}](https://punkscape.xyz/gallery27/punkscape/${ID})`, inline: true },
-      )
-      .setImage(`https://ipfs.punkscape.xyz/ipfs/QmaADSyXfiNhTqLKtRYxvGB4qJ7ZAR375Yh48SiCmcVYYE/${ID}/image.png`)
+  discordMessage.setColor(color)
+  discordMessage.setTitle(title)
+  discordMessage.setURL(config.url(saleData))
+
+  if (config.discord?.image) discordMessage.setImage(await config.discord.image(saleData))
+  if (config.discord?.thumbnail) discordMessage.setThumbnail(config.discord.thumbnail(saleData))
+  if (config.discord?.fields) discordMessage.addFields(...config.discord.fields(saleData))
 
   try {
     console.log('Sending sale discord message')
